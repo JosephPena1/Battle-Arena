4e4672f05a35eccdeb1ba5529ec13156d9c54d32
Nicholas Jordan <NicholasPhJordan@gmail.com>
2021-01-25 09:26:40 -0600

    Fixed shooting / In process of causing damage on impact

--- raygame/Bullet.cpp
+++ raygame/Bullet.cpp
@@ -1,5 +1,8 @@
 #include "Bullet.h"
 #include "Game.h"
+#include "Sprite.h"
+#include "raylib.h"
+#include <typeinfo>
 
 Bullet::Bullet(float x, float y, float collisionRadius, const char* spriteFilePath, float maxSpeed, MathLibrary::Vector2 velocity) : Actor(x, y, collisionRadius, spriteFilePath, maxSpeed, velocity)
 {
@@ -10,13 +13,20 @@ void Bullet::update(float deltaTime)
 {
 	Actor::update(deltaTime);
 
-	if (getWorldPosition().x < 0 || getWorldPosition().x > 50
-		|| getWorldPosition().y < 0 || getWorldPosition().y > 30)
-
+	if (getWorldPosition().x < 0 || getWorldPosition().x > 50 || getWorldPosition().y < 0 || getWorldPosition().y > 30)
 		Game::destroy(this);
 }
 
 void Bullet::draw()
 {
 	Actor::draw();
+}
+
+void Bullet::onCollision(Actor* other)
+{
+	if (typeid(other) == typeid(Enemy()))
+	{
+		other->takeDamage();
+		Actor::onCollision(other);
+	}
 }
